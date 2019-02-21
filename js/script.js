c508c92b162bf06a90fe8e70b43360ca0b48c5ce
solomon <solomioro@gmail.com>
2019-02-21 06:29:53 -0500

    html  calculator app

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var add = function(number1,number2){
+  return number1+number2;
+}
+var sub=function(number1, number2){
+    return number1-number2;
+  }
+var divide=function(number1, number2){
+    return number1/number2;}
+var multiple=function(number1, number2){
+      return number1*number2;
+  }
+
+$(document).ready(function(){
+  var number1=parseInt(prompt("enter first number"));
+  var number2= parseInt(prompt("enter second number"));
+  alert(-------)
+})
